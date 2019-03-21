b52a614215f40059de8b99ee013f8b061dc9eee2
Pratik Autee <36332526+prtkx2@users.noreply.github.com>
2019-03-21 16:08:09 +0530

    Added sound and start screen

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
 #include <graphics.h>
 #include <windows.h>
-
+#include <mmsystem.h>
 int isJumping=0;
+int sc=1;
+int lives=0;
+void dispBg();
+
+void dispStart(){
+PlaySound(TEXT("./mario.wav"),NULL,SND_LOOP|SND_ASYNC);
+readimagefile("./start_screen.jpg",0,0,1400,800);
+while(1){
+    if(GetAsyncKeyState(VK_SPACE))
+{
+    cleardevice();
+    dispBg();
+    break;
+}
+}
+
+}
 
 void dispBg(){
     readimagefile("./bg.jpg",0,0,1400,400);
@@ -12,7 +30,11 @@ void dispBg(){
     setfillstyle(1,10);
     bar(0,400,1400,800);
     readimagefile("./lives.jpg",50,600,350,740);
+    readimagefile("./score.jpg",800,500,1200,780);
+
 }
+
+
 
 void jumper(int x1,int y1,int x2,int y2){
     //setfillstyle(1,2);
@@ -27,12 +49,17 @@ void obs(int x1,int y1,int x2,int y2){
 
 int main(){
     initwindow(1400,800,"Jumping");
-    dispBg();
+    dispStart();
     readimagefile("./stand.jpg",140,300,260,400);
     while(1){
     for(int i=0;i<=1260;i=i+70){
             if(GetAsyncKeyState(VK_SPACE) && i/float(140)<=8.50 && i/float(140)>=7.00){
                 isJumping=1;
+                char score[50];
+                itoa(sc++,score,10);
+                setcolor(WHITE);
+                settextstyle(3,HORIZ_DIR,9);
+                outtextxy(1150,600,score);
                 //Hide
                 setfillstyle(1,9);
                 bar(140,300,280,400);
@@ -58,7 +85,10 @@ int main(){
                 bar(140,100,280,200);
         }
         if(isJumping==0 && i/70>=16)
-        {
+        {   lives++;
+            if(lives==3)
+            {lives=0;
+            sc=1;}
             readimagefile("./stand.jpg",140,300,260,400);
             obs(1300-i+60,360,1360-i+60,400);
             delay(5000);
