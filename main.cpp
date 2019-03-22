0f508a16e4da871127ef07efc0ad5c899953b320
sagarbagwe6699 <sagarbagwe6699@gmail.com>
2019-03-22 09:37:44 +0530

    Mostly final

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <mmsystem.h>
 int isJumping=0;
 int sc=1;
-int lives=0;
+int lives=3;
 void dispBg();
 
 void dispStart(){
@@ -29,8 +29,16 @@ void dispBg(){
     //bar(0,0,1400,400);
     setfillstyle(1,10);
     bar(0,400,1400,800);
-    readimagefile("./lives.jpg",50,600,350,740);
-    readimagefile("./score.jpg",800,500,1200,780);
+    if(lives==3){
+        readimagefile("./lives.jpg",50,600,350,740);
+    }
+    else if(lives==2){
+        readimagefile("./lives2.jpg",50,600,350,740);
+    }
+    else if(lives==1){
+        readimagefile("./lives1.jpg",50,600,350,740);
+    }
+    readimagefile("./score1.jpg",930,650,1130,690);
 
 }
 
@@ -43,15 +51,16 @@ void jumper(int x1,int y1,int x2,int y2){
 }
 
 void obs(int x1,int y1,int x2,int y2){
-    setfillstyle(1,15);
-    bar(x1,y1,x2,y2);
+    //setfillstyle(1,15);
+    //bar(x1,y1,x2,y2);
+    readimagefile("./mushroom.jpg",x1,y1,x2,y2);
 }
 
 int main(){
     initwindow(1400,800,"Jumping");
     dispStart();
     readimagefile("./stand.jpg",140,300,260,400);
-    while(1){
+    while(lives>=1){
     for(int i=0;i<=1260;i=i+70){
             if(GetAsyncKeyState(VK_SPACE) && i/float(140)<=8.50 && i/float(140)>=7.00){
                 isJumping=1;
@@ -59,7 +68,7 @@ int main(){
                 itoa(sc++,score,10);
                 setcolor(WHITE);
                 settextstyle(3,HORIZ_DIR,9);
-                outtextxy(1150,600,score);
+                outtextxy(1150,620,score);
                 //Hide
                 setfillstyle(1,9);
                 bar(140,300,280,400);
@@ -67,12 +76,14 @@ int main(){
                 jumper(140,100,280,200);
 
                 setfillstyle(1,15);
-                bar(220,360,260,400);
+                readimagefile("./mushroom.jpg",220,359,260,399);
+                //bar(220,360,260,400);
 
                 delay(100);
 
                 setfillstyle(1,15);
-                bar(220,360,280,400);
+                readimagefile("./mushroom.jpg",220,359,280,399);
+                //bar(220,360,280,400);
 
                 delay(10);
 
@@ -85,14 +96,15 @@ int main(){
                 bar(140,100,280,200);
         }
         if(isJumping==0 && i/70>=16)
-        {   lives++;
-            if(lives==3)
-            {lives=0;
-            sc=1;}
+        {   //lives++;
+            //if(lives==3)
+            //{lives=0;
+            //sc=1;}
             readimagefile("./stand.jpg",140,300,260,400);
             obs(1300-i+60,360,1360-i+60,400);
-            delay(5000);
+            delay(2000);
             cleardevice();
+            lives--;
             dispBg();
             break;
         }
@@ -107,6 +119,8 @@ int main(){
         bar(1280-i,300,1380-i,400);
     }
     }
+    readimagefile("./GO.jpg",0,0,1400,800);
+    delay(2000);
     getch();
     closegraph();
 }
